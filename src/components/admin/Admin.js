77d38e0ccec439749858d8a096daa62c0c242e5a
datntdev <datntdev02@gmail.com>
2023-05-26 16:28:06 +0700

    admin-page

---
+++ src/components/admin/Admin.js
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import { Scrollbars } from "react-custom-scrollbars-2";
+
+const Admin = () => {
+  return (
+    <div className="admin-container">
+      <Scrollbars
+        style={{ height: "100vh" }}
+        autoHide
+        // Hide delay in ms
+        autoHideTimeout={1000}
+        // Duration for hide animation in ms.
+        autoHideDuration={200}
+      >
+        
+        <div className="body-container">
+          <Outlet />
+        </div>
+      </Scrollbars>
+    </div>
+  );
+};
+
+export default Admin;
