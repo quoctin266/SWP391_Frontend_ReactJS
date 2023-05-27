e581b52d330c8450168c4118f741c0a8ef6488ff
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2023-05-27 10:50:17 +0700

    done manage route

--- src/components/manager/manageRouteTrip/ManageRouteTrip.js
+++ src/components/manager/manageRouteTrip/ManageRouteTrip.js
@@ -1,5 +1,37 @@
+import "./ManageRouteTrip.scss";
+import Accordion from "react-bootstrap/Accordion";
+import ManageRoute from "./detailManagement/ManageRoute";
+import ManageTrip from "./detailManagement/ManageTrip";
+import AssignTrip from "./detailManagement/AssignTrip";
+
 const ManageRouteTrip = () => {
-  return <div className="manage-routetrip-container">manage route trip</div>;
+  return (
+    <div className="manage-routetrip-container">
+      <div className="title">Manage route and trip</div>
+      <div className="route-trip-body">
+        <Accordion defaultActiveKey={["0"]} alwaysOpen>
+          <Accordion.Item eventKey="0" className="manage-route">
+            <Accordion.Header>Manage Route</Accordion.Header>
+            <Accordion.Body>
+              <ManageRoute />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1" className="manage-trip">
+            <Accordion.Header>Manage Trip</Accordion.Header>
+            <Accordion.Body>
+              <ManageTrip />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2" className="assign-trip">
+            <Accordion.Header>Assign Trip to Route</Accordion.Header>
+            <Accordion.Body>
+              <AssignTrip />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+    </div>
+  );
 };
 
 export default ManageRouteTrip;
---
+++ src/components/manager/manageRouteTrip/detailManagement/ManageTrip.js
@@ -0,0 +1,5 @@
+const ManageTrip = () => {
+  return <>fg</>;
+};
+
+export default ManageTrip;
