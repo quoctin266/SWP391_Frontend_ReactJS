9ac64af9361cd4dfec9db3cbaf953495bbab752a
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2023-05-28 11:45:57 +0700

    fix css

--- src/components/guestAndCustomer/auth/ResetPassword.js
+++ src/components/guestAndCustomer/auth/ResetPassword.js
@@ -1,19 +1,28 @@
-import "./ResetPassword.scss"
+import "./ResetPassword.scss";
 
 const ResetPassword = () => {
   return (
-  <div className="reset-password-container">
+    <div className="reset-password-container">
       <div className="ResetPasswordBox">
         <form action="" method="get" className="ResetPasswordForm">
           <h1>Reset Password</h1>
-          <div className="Input"><label for="RecentPassword">Recent Password</label><input type="password" name="RecentPassword"/></div>
-          <div className="Input"><label for="NewPassword">New Password</label><input type="password" name="NewPassword"/></div>
-          <div className="Input"><label for="ReEnterNewPassword">Re-Enter New Password</label><input type="password" name="ReEnterNewPassword"/></div>
+          <div className="Input">
+            <label htmlFor="RecentPassword">Recent Password</label>
+            <input type="password" id="RecentPassword" />
+          </div>
+          <div className="Input">
+            <label htmlFor="NewPassword">New Password</label>
+            <input type="password" id="NewPassword" />
+          </div>
+          <div className="Input">
+            <label htmlFor="ReEnterNewPassword">Re-Enter New Password</label>
+            <input type="password" id="ReEnterNewPassword" />
+          </div>
           <button className="Confirm">Confirm</button>
         </form>
       </div>
     </div>
-  )
+  );
 };
 
 export default ResetPassword;
